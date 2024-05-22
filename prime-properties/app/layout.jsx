0d3b4688888702cdb683b119d53216948bc9c100
Petya Ivanchova <pepivanchova@yahoo.com>
2024-05-22 16:20:53 +0300

    add next auth and google provider

--- prime-properties/app/layout.jsx
+++ prime-properties/app/layout.jsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/globals.css'
+import AuthProvider from '@/components/AuthProvider';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
@@ -10,6 +11,7 @@ export const metadata = {
 
 const MainLayout = ({children}) => {
     return (
+        <AuthProvider>
         <html lang='en'>
             <body>
                 <Navbar />
@@ -17,6 +19,7 @@ const MainLayout = ({children}) => {
                 <Footer />
             </body>
         </html>
+        </AuthProvider>
     )
 }
 
--- prime-properties/models/User.js
+++ prime-properties/models/User.js
@@ -21,7 +21,7 @@ const UserSchema = new Schema({
     ]
 }, {timestamps: true});
 
-const User = models.user || model('User', UserSchema);
+const User = models.User || model('User', UserSchema);
 
 export default User;
 
